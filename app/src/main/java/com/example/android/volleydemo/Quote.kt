00a89f7dd3c8171a250fc521b618fab91dac37cd
Wallaceness <ncw232@gmail.com>
2020-03-06 12:25:54 -0500

    Some ui and added message as primary key

--- app/src/main/java/com/example/android/volleydemo/Quote.kt
+++ app/src/main/java/com/example/android/volleydemo/Quote.kt
@@ -3,14 +3,14 @@ package com.example.android.volleydemo
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.security.MessageDigest
 
 @Entity(tableName="quotes_table")
-class Quote(@field:ColumnInfo(name = "message") val message:String,
+class Quote(@PrimaryKey @field:ColumnInfo(name = "message") val message:String,
             @field:ColumnInfo(name="author") val author: String,
             @field:ColumnInfo(name="keywords") val keywords: String,
             @field:ColumnInfo(name="profession") val profession:String,
             @field:ColumnInfo(name="nationality") val nationality: String,
             @field:ColumnInfo(name="authorBirth") val authorBirth:String,
             @field:ColumnInfo(name="authorDeath") val authorDeath:String) {
-    @PrimaryKey(autoGenerate = true) var id=0
 }
--- app/src/main/java/com/example/android/volleydemo/ViewModel/QuotesDao.kt
+++ app/src/main/java/com/example/android/volleydemo/ViewModel/QuotesDao.kt
@@ -3,12 +3,13 @@ package com.example.android.volleydemo.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.android.volleydemo.Quote
 
 @Dao()
 interface QuotesDao {
-    @Insert()
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(quote: Quote?)
 
     @Query("Select * from quotes_table")
